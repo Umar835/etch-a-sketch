c4d009f89ad967344748be3d011ee2a5b0fe1d14
Umar Hussain <umarhussain771@gmail.com>
2022-09-01 15:26:37 +0100

    Addes hover effect to the grid

--- main.js
+++ main.js
@@ -3,16 +3,24 @@ const gridSize = 16
 const container = document.querySelector('.container')
 
 function createGrid(gridSize){
+    //creates grid rows
     for(let i = 0; i < gridSize; i++){
       const row = document.createElement('div')
       row.classList.add('grid-row')
-
+      
+    //creates the number of grid boxes
       for(let j = 0; j < gridSize; j++){
         const gridBox = document.createElement('div')
         gridBox.classList.add('grid-box')
+        // changing background color on grid.
+        gridBox.addEventListener('mouseenter', () =>{
+         gridBox.style.backgroundColor ='black'  
+        })
         row.appendChild(gridBox)
       }
       container.appendChild(row)
     }
 }
 createGrid(gridSize)
+
+
